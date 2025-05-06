aa7c1a4f6a5d88b05f98d5c26069b10d427c136c
Ujjwal Gupta <150505461+Ujjwagupta1302@users.noreply.github.com>
2025-05-06 15:00:04 +0530

    Mlruns code added

--- networksecurity/components/model_trainer.py
+++ networksecurity/components/model_trainer.py
@@ -1,5 +1,6 @@
 import os
 import sys
+import mlflow 
 
 from networksecurity.exception.exception import NetworkSecurityException 
 from networksecurity.logging.logger import logging
@@ -34,6 +35,33 @@ class ModelTrainer:
             self.data_transformation_artifact = data_transformation_artifact
         except Exception as e :
             raise NetworkSecurityException(e,sys) 
+        
+    def track_mlflow(self,best_model,classificationmetric):
+        #mlflow.set_registry_uri("https://dagshub.com/krishnaik06/networksecurity.mlflow")
+        #tracking_url_type_store = urlparse(mlflow.get_tracking_uri()).scheme
+        with mlflow.start_run():
+            f1_score=classificationmetric.f1_score
+            precision_score=classificationmetric.precision_score
+            recall_score=classificationmetric.recall_score
+
+            
+
+            mlflow.log_metric("f1_score",f1_score)
+            mlflow.log_metric("precision",precision_score)
+            mlflow.log_metric("recall_score",recall_score)
+            mlflow.sklearn.log_model(best_model,"model")
+            # Model registry does not work with file store
+            '''
+            if tracking_url_type_store != "file":
+
+                # Register the model
+                # There are other ways to use the Model Registry, which depends on the use case,
+                # please refer to the doc for more information:
+                # https://mlflow.org/docs/latest/model-registry.html#api-workflow
+                mlflow.sklearn.log_model(best_model, "model", registered_model_name=best_model)
+            else:
+                mlflow.sklearn.log_model(best_model, "model")
+            '''
         
     def train_model(self, x_train, y_train, x_test, y_test) :
         models = {
@@ -85,8 +113,12 @@ class ModelTrainer:
         y_train_pred = best_model.predict(x_train) 
         classification_train_metric = get_classification_score(y_true = y_train, y_pred = y_train_pred)
 
+        self.track_mlflow(best_model,classification_train_metric)
+
+
         y_test_pred = best_model.predict(x_test) 
         classification_test_metric = get_classification_score(y_true = y_test, y_pred = y_test_pred)
+        self.track_mlflow(best_model,classification_test_metric)
 
         preprocessor = load_object(file_path=self.data_transformation_artifact.transformed_object_file_path)
 
